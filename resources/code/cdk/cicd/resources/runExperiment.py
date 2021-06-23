30cd8e6884e7ca7b721dccf90e5df2ce7e202a64
David Christian <dchristian3188@gmail.com>
2021-06-23 14:48:03 -0700

    Cicd (#32)
    * updating initial CICD code
    * adding cicd workshop files
    * adding code resources
    
    Co-authored-by: David Christian <chradavi@amazon.com>

---
+++ resources/code/cdk/cicd/resources/runExperiment.py
@@ -0,0 +1,52 @@
+import boto3
+import sys
+import time
+
+fisClient = boto3.client('fis')
+
+
+def main(argv):
+    experiment_name = argv[0]
+    templateID = getExperimentTemplateID(experiment_name)
+    if(templateID == None):
+        print("Could not find experiment template for [" + experiment_name + "]")
+        sys.exit()
+
+    print("Starting new [" + experiment_name + "] experiment")
+    startExperimentResponse = fisClient.start_experiment(
+        experimentTemplateId=templateID
+    )
+
+    experimentID = startExperimentResponse['experiment']['id']
+    experimentStatus = startExperimentResponse['experiment']['state']['status']
+    print("Started experiment ID [" + experimentID + "]")
+    print("Current status is [" + experimentStatus + "]")
+
+    while(experimentStatus not in ['completed', 'stopped' , 'failed']):
+        print("Waiting for experiment to complete. Current Status is [" + experimentStatus + "]")
+        time.sleep(30)
+        getExperimentResponse = fisClient.get_experiment(
+            id = experimentID
+        )
+        experimentID = getExperimentResponse['experiment']['id']
+        experimentStatus = getExperimentResponse['experiment']['state']['status']
+
+    print("Experiment Complete with status [" + experimentStatus + "]")
+    print("Result was [" + getExperimentResponse['experiment']['state']['reason'] + "]")
+
+    if(experimentStatus == 'completed'):
+        return 0
+    else: 
+        return -1
+
+def getExperimentTemplateID(experiment):
+    print("Getting experiment template for " + experiment)
+    templates = fisClient.list_experiment_templates()
+    for template in templates['experimentTemplates']:
+        if(template['tags']['Name'] == experiment):
+            print("Found template with ID [" + template['id'] + "]")
+            return template['id']
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- workshop/content/30 Basic Content/cicd/_index.en.md
+++ workshop/content/30 Basic Content/cicd/_index.en.md
@@ -4,4 +4,4 @@ date =  2021-04-14T17:25:37-06:00
 weight = 5
 +++
 
-Lorem Ipsum.
+In this section, we will discuss how to integrate a Fault Injection Simulator experiment with a CICD pipeline.
