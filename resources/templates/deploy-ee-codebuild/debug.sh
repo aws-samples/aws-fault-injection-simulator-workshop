7e47308c8cdce74ecd19823a3a15d8e801a7763b
Rudolf Potucek <50185754+rudpot@users.noreply.github.com>
2021-11-23 00:11:52 -0700

    Update debug script (#252)

--- resources/templates/deploy-ee-codebuild/debug.sh
+++ resources/templates/deploy-ee-codebuild/debug.sh
@@ -2,10 +2,10 @@
 
 AWS_ACCOUNT=$( aws sts get-caller-identity --query 'Account' --output text )
 
-echo "Exporting cloudformation stacks that were created and their state"
-aws cloudformation list-stacks > ee-debug.${AWS_ACCOUNT}.stacks.txt
+echo "${AWS_ACCOUNT}: Exporting cloudformation stacks that were created and their state"
+aws cloudformation list-stacks > ee-debug.${AWS_ACCOUNT}.stacks.txt 2>&1
 
-echo "Exporting codebuild history"
+echo "${AWS_ACCOUNT}: Exporting codebuild history"
 LOG_GROUPS=$( aws logs describe-log-groups --log-group-name-prefix /aws/codebuild/ --query 'logGroups[*].logGroupName' --output text )
 for ii in ${LOG_GROUPS}; do
   LOG_STREAMS=$( aws logs describe-log-streams --log-group-name ${ii} --query 'logStreams[*].logStreamName' --output text )
@@ -15,3 +15,24 @@ for ii in ${LOG_GROUPS}; do
     aws logs get-log-events --log-group-name ${ii} --log-stream-name ${jj} >> ee-debug.${AWS_ACCOUNT}.codebuild.txt 2>&1 
   done
 done
+
+echo "${AWS_ACCOUNT}: Exporting custom resource lambda history"
+LOG_GROUPS=$( aws logs describe-log-groups --log-group-name-prefix /aws/lambda/mod- --query 'logGroups[*].logGroupName' --output text )
+for ii in ${LOG_GROUPS}; do
+  LOG_STREAMS=$( aws logs describe-log-streams --log-group-name ${ii} --query 'logStreams[*].logStreamName' --output text )
+  for jj in ${LOG_STREAMS}; do
+    echo === ${ii} === ${jj} ===
+    echo === ${ii} === ${jj} === >> ee-debug.${AWS_ACCOUNT}.custom-lambda.txt
+    aws logs get-log-events --log-group-name ${ii} --log-stream-name ${jj} >> ee-debug.${AWS_ACCOUNT}.custom-lambda.txt 2>&1 
+  done
+done
+
+echo "${AWS_ACCOUNT}: Exporting role information"
+for ii in TeamRole OpsRole; do
+  echo === ${ii} === >> ee-debug.${AWS_ACCOUNT}.roles.txt
+  aws iam get-role --role-name TeamRole >> ee-debug.${AWS_ACCOUNT}.roles.txt 2>&1 
+  aws iam list-role-policies --role-name TeamRole >> ee-debug.${AWS_ACCOUNT}.roles.txt 2>&1 
+  aws iam list-attached-role-policies --role-name TeamRole >> ee-debug.${AWS_ACCOUNT}.roles.txt 2>&1 
+done
+
+
