774d2b149a4d3856ffcbd8585dd35ead948efc66
Rudolf Potucek <rudpot@amazon.com>
2022-06-03 13:37:35 -0600

    Fix RDS stack

--- resources/templates/rds/lib/fis-stack-rds-aurora.ts
+++ resources/templates/rds/lib/fis-stack-rds-aurora.ts
@@ -1,12 +1,13 @@
-import * as cdk from '@aws-cdk/core';
-import * as ec2 from '@aws-cdk/aws-ec2';
-import * as rds from '@aws-cdk/aws-rds';
-import { InstanceType } from '@aws-cdk/aws-ec2';
-import * as ssm from '@aws-cdk/aws-ssm';
-import * as secretsmanager from '@aws-cdk/aws-secretsmanager';
+import * as cdk            from 'aws-cdk-lib';
+import {Construct}         from 'constructs';
+import * as ec2            from 'aws-cdk-lib/aws-ec2';
+import * as rds            from 'aws-cdk-lib/aws-rds';
+import { InstanceType }    from 'aws-cdk-lib/aws-ec2';
+import * as ssm            from 'aws-cdk-lib/aws-ssm';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 
 export class FisStackRdsAurora extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const vpc = ec2.Vpc.fromLookup(this, 'FisVpc', { 
