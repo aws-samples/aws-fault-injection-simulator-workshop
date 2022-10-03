013e339408a55307ab9241399dceacf381e4c239
Rudolf Potucek <rudpot@amazon.com>
2022-10-03 09:02:31 -0700

    Clean up template generators

--- resources/code/scripts/cheat-03.sh
+++ resources/code/scripts/cheat-03.sh
@@ -6,9 +6,17 @@ echo "Create first experiment template"
 export REGION=$(aws ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].[RegionName]')
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account')
 
-# install envsubst if needed
-sudo yum install -y gettext
+# check if we already have a correctly named template
+export TEMPLATE_NAME="FisWorkshopExp1Run1"
 
-cat cheat-03/template.json | envsubst > /tmp/cheat-03.json
+EXISTS=$( aws fis list-experiment-templates --query "experimentTemplates[?tags.Name=='${TEMPLATE_NAME}'].id" --output text )
 
-AWS_PAGER="" aws fis create-experiment-template --cli-input-json file:///tmp/cheat-03.json
+if [ -z "$EXISTS" ]; then
+
+    # install envsubst if needed
+    sudo yum install -y gettext
+
+    cat cheat-03/template.json | envsubst > /tmp/cheat-03.json
+
+    AWS_PAGER="" aws fis create-experiment-template --cli-input-json file:///tmp/cheat-03.json
+fi
--- resources/code/scripts/cheat-04.sh
+++ resources/code/scripts/cheat-04.sh
@@ -6,9 +6,17 @@ echo "Create Linux SSM CPU stress template"
 export REGION=$(aws ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].[RegionName]')
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account')
 
-# install envsubst if needed
-sudo yum install -y gettext
+# check if we already have a correctly named template
+export TEMPLATE_NAME="LinuxBurnCPUviaSSM"
 
-cat cheat-04/template.json | envsubst > /tmp/cheat-04.json
+EXISTS=$( aws fis list-experiment-templates --query "experimentTemplates[?tags.Name=='${TEMPLATE_NAME}'].id" --output text )
 
-AWS_PAGER="" aws fis create-experiment-template --cli-input-json file:///tmp/cheat-04.json
+if [ -z "$EXISTS" ]; then
+
+    # install envsubst if needed
+    sudo yum install -y gettext
+
+    cat cheat-04/template.json | envsubst > /tmp/cheat-04.json
+
+    AWS_PAGER="" aws fis create-experiment-template --cli-input-json file:///tmp/cheat-04.json
+fi
