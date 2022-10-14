290976c3261f82767e5c86191229bb1b0400f680
Rudolf Potucek <rudpot@amazon.com>
2022-10-13 19:17:50 -0600

    WIP: testing FIS/SSM cheat

--- resources/code/scripts/cheat-09.sh
+++ resources/code/scripts/cheat-09.sh
@@ -6,7 +6,7 @@ echo "Create FIS template for DOP313"
 export DOP313_TEMPLATE_NAME=FisWorkshopDop313
 
 export DOP313_SSM_DOCUMENT_NAME=TerminateAsgInstancesWithSsm
-export DOP313_SSM_DOCUMENT_ARN=arn:aws:ssm:${REGION}:${ACCOUNT_ID}:document/${SSM_DOCUMENT_NAME}
+export DOP313_SSM_DOCUMENT_ARN=arn:aws:ssm:${REGION}:${ACCOUNT_ID}:document/${DOP313_SSM_DOCUMENT_NAME}
 export REGION=$(aws ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].[RegionName]')
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account')
 
@@ -20,7 +20,7 @@ export SSM_ROLE_NAME=FisWorkshopSsmEc2DemoRole
 export FIS_ROLE_ARN=$( aws iam list-roles --query "Roles[?RoleName=='${FIS_ROLE_NAME}'].Arn" --output text )
 export SSM_ROLE_ARN=$( aws iam list-roles --query "Roles[?RoleName=='${SSM_ROLE_NAME}'].Arn" --output text )
 
-EXISTS=$( aws fis list-experiment-templates --query "experimentTemplates[?tags.Name=='${TEMPLATE_NAME}'].id" --output text )
+EXISTS=$( aws fis list-experiment-templates --query "experimentTemplates[?tags.Name=='${DOP313_TEMPLATE_NAME}'].id" --output text )
 
 if [ -z "$EXISTS" ]; then
 
