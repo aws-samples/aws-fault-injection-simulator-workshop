7dbcc897a73ff398f6239e6761457ffd80a3218e
Rudolf Potucek <50185754+rudpot@users.noreply.github.com>
2022-11-24 12:16:08 -0700

    Update list instances (#383)

--- workshop/content/030_basic_content/040_ssm/050_direct_automation/list-instances.sh
+++ workshop/content/030_basic_content/040_ssm/050_direct_automation/list-instances.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
+
+[ -z "$REGIOH" ] && REGION=$(aws ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].[RegionName]')
+[ -z "$ACCOUNT_ID" ] && ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account')
+
+IMPACT_ASG_NAME=$( aws cloudformation describe-stack-resources --stack-name FisStackAsg --query "StackResources[?ResourceType=='AWS::AutoScaling::AutoScalingGroup'].PhysicalResourceId" --output text  )
+
 aws ec2 describe-instances \
 --query "Reservations[*].Instances[*].{ID:InstanceId,AZ:Placement.AvailabilityZone,Type:InstanceType,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}"  \
---filters "Name=instance-state-name,Values=running" "Name=tag:Name,Values='FisStackAsg/ASG'"  \
+--filters "Name=instance-state-name,Values=running" "Name=tag:Name,Values='FisStackAsg/ASG'" \
 --output table
+
+aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names ${IMPACT_ASG_NAME} --query "AutoScalingGroups[*].AvailabilityZones" \
+--output table
+
